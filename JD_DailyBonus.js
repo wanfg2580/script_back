6bf5f4bfe71366d376f2852da4dd47fc49e4230b
wangjingming <wangjingming@bixin.cn>
2021-10-12 10:22:56 +0800

    update

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -130,8 +130,8 @@ var merge = {};
 var KEY = '';
 
 
-const Faker = require('./utils/JDSignValidator')
-const zooFaker = require('./utils/JDJRValidator_Pure')
+const Faker = require('./JDSignValidator')
+const zooFaker = require('./JDJRValidator_Pure')
 let fp = '', eid = '', md5
 $nobyda.get = zooFaker.injectToRequest2($nobyda.get.bind($nobyda), 'channelSign')
 $nobyda.post = zooFaker.injectToRequest2($nobyda.post.bind($nobyda), 'channelSign')
