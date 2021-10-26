a3025f4cfd03464461fc258c6482270f70677478
wangjingming <wangjingming@bixin.cn>
2021-10-26 11:23:09 +0800

    update

--- jd_big_winner.js
+++ jd_big_winner.js
@@ -6,24 +6,24 @@
 =================================Quantumultx=========================
 [task_local]
 #省钱大赢家之翻翻乐
-20 * * * * https://jdsharedresourcescdn.azureedge.net/jdresource/jd_big_winner.js, tag=省钱大赢家之翻翻乐, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+20 0,6-23 * * * jd_big_winner.js, tag=省钱大赢家之翻翻乐, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
 =================================Loon===================================
 [Script]
-cron "20 * * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_big_winner.js,tag=省钱大赢家之翻翻乐
+cron "20 0,6-23 * * *" script-path=jd_big_winner.js,tag=省钱大赢家之翻翻乐
 
 ===================================Surge================================
-省钱大赢家之翻翻乐 = type=cron,cronexp="20 * * * *",wake-system=1,timeout=3600,script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_big_winner.js
+省钱大赢家之翻翻乐 = type=cron,cronexp="20 0,6-23 * * *",wake-system=1,timeout=3600,script-path=jd_big_winner.js
 
 ====================================小火箭=============================
-省钱大赢家之翻翻乐 = type=cron,script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_big_winner.js, cronexpr="20 * * * *", timeout=3600, enable=true
+省钱大赢家之翻翻乐 = type=cron,script-path=jd_big_winner.js, cronexpr="20 0,6-23 * * *", timeout=3600, enable=true
  */
 const $ = new Env('省钱大赢家之翻翻乐');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
-let cookiesArr = [], cookie = '', message = '', linkId = 'DA4SkG7NXupA9sksI00L0g', fflLinkId = 'YhCkrVusBVa_O2K-7xE6hA';
+let cookiesArr = [], cookie = '', message = '', linkId = 'PFbUR7wtwUcQ860Sn8WRfw', fflLinkId = 'YhCkrVusBVa_O2K-7xE6hA';
 const JD_API_HOST = 'https://api.m.jd.com/api';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -60,12 +60,12 @@ const len = cookiesArr.length;
     //if ($.isNode()) await notify.sendNotify($.name, message);
   }
 })()
-  .catch((e) => {
-    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
-  })
-  .finally(() => {
-    $.done();
-  })
+    .catch((e) => {
+      $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+    })
+    .finally(() => {
+      $.done();
+    })
 
 async function main() {
   try {
