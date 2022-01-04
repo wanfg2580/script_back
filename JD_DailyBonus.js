131245ec3f7d369a9b3402ad2283db54ed8f9ed7
wangjingming <wangjingming@bixin.cn>
2022-01-04 11:16:14 +0800

    update

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -16,7 +16,7 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = `[{"cookie":"pt_pin=18189815038_p;pt_key=AAJhznX_ADD9zdLilNZim61QUv8ivK5e3c9nkwb2fSjsXyYn9FmOX7xP0xHaEgjXNPI0JmxIwlM;","jrBody":""}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 
 var LogDetails = false; //是否开启响应日志, true则开启
